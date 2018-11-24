7a836b05b752ea79ee8068e1dd9e7803093e0730
ilian kostadinov <ilian@mail.kostadinovi.info>
2018-11-24 13:13:02 +0200

    Solution of Ex3.1

---
+++ Ex3.1.py
@@ -0,0 +1,19 @@
+!#/usr/bin/python3
+
+"""
+   Whrite a function named right_justify that takes a  string named s as a parameter 
+   and prints the string with enough leading spaces so that  the last  letter of the 
+   string is in column 70 of the display
+
+"""
+
+def right_justify(s):
+
+    print(s)
+
+if __name__ == '__main__':
+
+    print("Please enter a string")
+    inStr = input()
+    right_justify(inStr)
+
