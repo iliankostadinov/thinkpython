1d79061f99ac95cc76f0cb49ab0561286f2250ba
ilian.kostadinov@gmail.com <ilian.kostadinov@gmail.com>
2018-12-05 11:17:19 +0200

    Begging of solution of Ackkermann function

---
+++ Chapter6/Ex6.2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Write a funciton named ack that evaluates the Ackermann function
+
+def ack(m, n):
+    if not isinstance(m, int) and int(m) <  0:
+        print("Value of m should be positive integer")
+        return None
+    if not isinstance(n, int) and int(n) < 0:
+        print("Value of n should be positive integer")
+        return None
+
+
+if __name__ == '__main__':
+    m = input("Please enter positive integer ")
+    n = input("Please enter positive integer ")
+    ack(m, n)
