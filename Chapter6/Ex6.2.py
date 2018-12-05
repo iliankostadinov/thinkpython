536d01d5800afed344124261da945a9415e0ef34
ilian kostadinov <ilian.kostadinov@gmail.com>
2018-12-05 11:22:38 +0200

    Fixed some spaces typo

--- Chapter6/Ex6.2.py
+++ Chapter6/Ex6.2.py
@@ -2,8 +2,9 @@
 
 # Write a funciton named ack that evaluates the Ackermann function
 
+
 def ack(m, n):
-    if not isinstance(m, int) and int(m) <  0:
+    if not isinstance(m, int) and int(m) < 0:
         print("Value of m should be positive integer")
         return None
     if not isinstance(n, int) and int(n) < 0:
