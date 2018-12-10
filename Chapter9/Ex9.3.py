1b120fbd7edcd2c023069df5fd7f960de939594e
ilian kostadinov <ilian.kostadinov@gmail.com>
2018-12-11 00:40:52 +0200

    solution of ex9.3

--- Chapter9/Ex9.3.py
+++ Chapter9/Ex9.3.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
 """
- Write a function named uses_only that takes a word and a string of forbidden
- letters and then returns True if the word doesn't use any of the forbidden 
+ Write a function named avoids that takes a word and a string of forbidden
+ letters and then returns True if the word doesn't use any of the forbidden
  letters.
 """
 
 
-def uses_only(word, string):
-    for ch
+def avoids(word, string):
+    for chars in string:
+        if chars in word:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    fin = open('words.txt')
+    string = input("Please enter letters which shouldn't present\n")
+    word_count = 0  # number of words which doesn't contain letters from string
+    for line in fin:
+        word = line.strip()
+        if avoids(word, string):
+            word_count += 1
+    print("Count of words that doesn't contain this letters is ", word_count)
