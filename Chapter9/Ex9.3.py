c49c2e84f7c7932c82beb5ffc71bbb54606120e3
ilian kostadinov <ilian.kostadinov@gmail.com>
2018-12-10 11:38:15 +0200

    Start solution of ex 9.3

---
+++ Chapter9/Ex9.3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+ Write a function named uses_only that takes a word and a string of forbidden
+ letters and then returns True if the word doesn't use any of the forbidden 
+ letters.
+"""
+
+
+def uses_only(word, string):
+    for ch
