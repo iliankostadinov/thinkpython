4b197984ad1e6e5fa468863d5051a59671ae7445
ilian.kostadinov@gmail.com <ilian.kostadinov@gmail.com>
2020-01-14 12:44:49 +0200

    Soluton of Exercise 15.1

--- Chapter15/Exercise_15_1.py
+++ Chapter15/Exercise_15_1.py
@@ -1,152 +1,141 @@
 #!/usr/bin/env python3
 
-"""Write a definition for a class named Circle with attributes center and
-radius where center is a Point object and radius is a number.
-Instantiate a Circle object that represents a circle with its center at
-( 150, 100 ) and radius 75. Write a function named point_in_circle that takes a
-Circle and a Point and returns True if the Point lies in or on the boundary of
-the circle. Write a function named rect_in_circle that takes a Circle and a
-Rectangle and returns True if the Rectangle lies entirely in or on the boundary
-of the circle. Write a function named rect_circle_overlap that takes a Circle
-and a Rectangle and returns True if any of the corners of the Rectangle fall
-inside the circle. Or as a more challenging version, return True if any part
-of the Rectangle falls inside the circle.
-"""
-
 
 class Point:
-    """Represent a point in 2D space.
+    """Represent a poing in 2D space
+
     attributes: x, y
     """
 
 
 class Circle:
-    """Represent a circle in 2-D space.
-    atributes: center which is Point object and radius
+    """Present a circle in 2D space with
+    point object for center and number for radius
+
+    attributes: center, radius
     """
 
 
 class Rectangle:
-    """Present a Rectangle in 2-D space.
-    atributes: points in opposite corners
+    """Present a rectangle in 2D space with
+    point object for corner and numbers for widht and height
+
+    attributes: corner, height, width
     """
 
 
-def point_in_circle(cir, poin):
-    """Return True if point lies in boundary of the circle
+def find_center(rect):
+    """Takes a rectangle and return
+    point which is its center
+
+    rect: Rectangle object
+    """
+
+    p = Point()
+    p.x = rect.corner.x + rect.width/2
+    p.y = rect.corner.y + rect.height/2
+    return p
+
+
+def point_in_circle(cir, p):
+    """Takes a Circle and a Point and returns True if the
+    Point lies in or on the boundary of the circle
 
     cir: Circle object
-    poin: Point ojbect
+    p: Point object
     """
-    if abs(cir.center.x - poin.x) > cir.radius:
+
+    if abs(cir.center.x-p.x) > cir.radius:
         return False
-    elif abs(cir.center.y - poin.y) > cir.radius:
+    if abs(cir.center.y-p.y) > cir.radius:
         return False
-    else:
-        return True
+
+    return True
 
 
 def rect_in_circle(cir, rect):
-    """Return True if Rectangle lies in boundary of the circle
+    """Takes a Circle and a Rectangle and returns True if
+       the Rectangle lies entirely in or on the boundary of the circle
 
-    cir: Circle object
-    rect: Rectangle object
+       cir: Circle object
+       rect: Rectangle object
     """
-    corner_1 = Point()
-    corner_1.x = rect.p1.x
-    corner_1.y = rect.p1.y
-    corner_2 = Point()
-    corner_2.x = rect.p1.x
-    corner_2.y = rect.p2.y
-    corner_3 = Point()
-    corner_3.x = rect.p2.x
-    corner_3.y = rect.p1.y
-    corner_4 = Point()
-    corner_4.x = rect.p2.y
-    corner_4.y = rect.p1.x
 
-    if point_in_circle(cir, corner_1):
-        pass
-    else:
+    corner1 = corner2 = corner3 = corner4 = Point()
+    corner1 = rect.corner
+    corner2.x = corner1.x+rect.width
+    corner2.y = corner1.y
+    corner3.x = corner1.x+rect.width
+    corner3.y = corner1.y+rect.height
+    corner4.x = corner1.x
+    corner4.y = corner1.y+rect.height
+
+    if not point_in_circle(cir, corner1):
         return False
-    if point_in_circle(cir, corner_2):
-        pass
-    else:
+    if not point_in_circle(cir, corner2):
         return False
-    if point_in_circle(cir, corner_3):
-        pass
-    else:
+    if not point_in_circle(cir, corner3):
         return False
-    if point_in_circle(cir, corner_4):
-        pass
-    else:
+    if not point_in_circle(cir, corner4):
         return False
 
     return True
 
 
 def rect_circle_overlap(cir, rect):
-    """Return True if any part of Rectangle overlap circle
+    """Takes a Circle and a Rectangle and returns True if
+       any of the corners of the Rectangle fall inside the circle
 
-    cir: Circle object
-    rect: Rectangle object
+       cir: Circle object
+       rect: Rectangle object
     """
 
-    corner_1 = Point()
-    corner_1.x = rect.p1.x
-    corner_1.y = rect.p1.y
-    corner_2 = Point()
-    corner_2.x = rect.p1.x
-    corner_2.y = rect.p2.y
-    corner_3 = Point()
-    corner_3.x = rect.p2.x
-    corner_3.y = rect.p1.y
-    corner_4 = Point()
-    corner_4.x = rect.p2.y
-    corner_4.y = rect.p1.x
+    corner1 = corner2 = corner3 = corner4 = Point()
+    corner1 = rect.corner
+    corner2.x = corner1.x+rect.width
+    corner2.y = corner1.y
+    corner3.x = corner1.x+rect.width
+    corner3.y = corner1.y+rect.height
+    corner4.x = corner1.x
+    corner4.y = corner1.y+rect.height
 
-    if point_in_circle(cir, corner_1):
+    if point_in_circle(cir, corner1):
         return True
-    if point_in_circle(cir, corner_2):
+    if point_in_circle(cir, corner2):
         return True
-    if point_in_circle(cir, corner_3):
+    if point_in_circle(cir, corner3):
         return True
-    if point_in_circle(cir, corner_4):
+    if point_in_circle(cir, corner4):
         return True
 
     return False
 
 
-if __name__ == "__main__":
-    center = Point()
-    center.x = 15
-    center.y = 10
+def main():
+    box = Rectangle()
+    box.width = 100.0
+    box.height = 200.0
+    box.corner = Point()
+    box.corner.x = 50.0
+    box.corner.y = 50.0
 
-    p = Point()
-    p.x = 20
-    p.y = 30
+    print(box.corner.x)
+    print(box.corner.y)
 
-    c = Circle()
-    c.center = center
-    c.radius = 75
+    circle = Circle
+    circle.center = Point()
+    circle.center.x = 150.0
+    circle.center.y = 100.0
+    circle.radius = 75.0
 
-    p1 = Point()
-    p2 = Point()
-    p1.x = 20
-    p1.y = 30
-    p2.x = 10
-    p2.y = 5
+    print(circle.center.x)
+    print(circle.center.y)
+    print(circle.radius)
 
-    rect = Rectangle()
-    rect.p1 = p1
-    rect.p2 = p2
+    print(point_in_circle(circle, box.corner))
+    print(rect_in_circle(circle, box))
+    print(rect_circle_overlap(circle, box))
 
-    if point_in_circle(c, p):
-        print("Point is in circle!")
-    else:
-        print("Point is not in circle")
 
-    if rect_in_circle(c, rect):
-        print("Rectangle is in circle!")
-    else:
-        print("Rectangle is not in circle")
+if __name__ == '__main__':
+    main()
